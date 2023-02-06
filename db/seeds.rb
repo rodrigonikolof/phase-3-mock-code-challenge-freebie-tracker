0099a23757d550e5b5f7b535ae19d1adc656294c
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2023-02-06 13:31:50 +1100

    Done with assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,5 +17,13 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+Freebie.create(item_name: "Keychain", value: 2, company_id: 1, dev_id: 2)
+Freebie.create(item_name: "Keychain", value: 2, company_id: 1, dev_id: 1)
+Freebie.create(item_name: "Phone Charger", value: 10, company_id: 3, dev_id: 2)
+Freebie.create(item_name: "Pencil", value: 1, company_id: 4, dev_id: 3)
+Freebie.create(item_name: "Knife", value: 4, company_id: 3, dev_id: 4)
+Freebie.create(item_name: "Job Offer", value: 60_000, company_id: 1, dev_id: 3)
+Freebie.create(item_name: "Job Offer", value: 200_000, company_id: 3, dev_id: 1)
+
 
 puts "Seeding done!"
